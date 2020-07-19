e904bdbb42938f7cc7013b82af60bce6871fe649
Evan Layher <evan.layher@psych.ucsb.edu>
2020-07-19 01:44:26 -0700

    fixed minor error

--- normalized_criterion.m
+++ normalized_criterion.m
@@ -1,76 +1,76 @@
-function [normCs, residuals, slope, intercept, r, r2] = normalized_criterion(dVec, cVec)
-% normalized_criterion.m: Remove explained variance of d' from criterion values
-% function [normCs, residuals, slope, intercept, r, r2] = normalized_criterion(dVec, cVec)
-% 
+function [nc, resid, slope, int, r, r2] = normalized_criterion(cVec, dVec)
+% normalized_criterion.m: Residualize c against d' to get normalized c values
+%
+% function [nc, resid, slope, int, r, r2] = normalized_criterion(cVec, dVec)
+%
 % Created: 04/10/2018, Evan Layher
 % Revised: 05/02/2018, Evan Layher % Added residuals output
-% 
+% Revised: 02/16/2020, Evan Layher % Fixed cSq calculation & minor updates
+%
 % --- LICENSE INFORMATION --- %
 % Modified BSD-2 License - for Non-Commercial Use Only
 %
-% Copyright (c) 2018, The Regents of the University of California
+% Copyright (c) 2018-20, The Regents of the University of California
 % All rights reserved.
 %
-% Redistribution and use in source and binary forms, with or without modification, are 
+% Redistribution and use in source and binary forms, with or without modification, are
 % permitted for non-commercial use only provided that the following conditions are met:
 %
-% 1. Redistributions of source code must retain the above copyright notice, this list 
+% 1. Redistributions of source code must retain the above copyright notice, this list
 %    of conditions and the following disclaimer.
 %
-% 2. Redistributions in binary form must reproduce the above copyright notice, this list 
-%    of conditions and the following disclaimer in the documentation and/or other 
+% 2. Redistributions in binary form must reproduce the above copyright notice, this list
+%    of conditions and the following disclaimer in the documentation and/or other
 %    materials provided with the distribution.
 %
-% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
-% EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES 
-% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
-% SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, 
-% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED 
-% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
-% OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
-% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY 
+% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+% EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+% OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+% SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+% INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+% TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+% OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+% CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
 % WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 %
-% For permission to use for commercial purposes, please contact UCSB's Office of 
+% For permission to use for commercial purposes, please contact UCSB's Office of
 % Technology & Industry Alliances at 805-893-5180 or info@tia.ucsb.edu.
 % --------------------------- %
 %
-% Signal detection theory: Adjust criterion values to account for relationship with d'
-% Use linear regression to find relationship between criterion and d'
-% The regression residuals are added to mean criterion to get normalized criterion values.
+% Signal detection theory: Adjust criterion (c) values to account for relationship with d'
+% Use linear regression to find relationship between c and d'
+% The regression residuals are added to mean c to get normalized c values
 %
 % Inputs: 2 vectors of equal length
-% (1) dVec: Vector of d' values
-% (2) cVec: Vector of criterion values
+% (1) cVec: vector of c values
+% (2) dVec: vector of d' values
 %
-% Outputs (6): normCs, residuals, slope, intercept, r, r2
-% (1) normCs: normalized criterion values (residuals + criterion mean)
-% (2) residuals: residual values only
+% Outputs:
+% (1) nc: normalized c values (residuals + c mean)
+% (2) resid: residuals
 % (3) slope: regression slope
-% (4) intercept: regression intercept
+% (4) int: regression intercept
 % (5) r: Pearson's r
-% (6) r2: Pearson's r^2
+% (6) r2: Pearson's r squared
 
-if length(dVec) ~= length(cVec)
-    fprintf('VECTORS MUST BE OF SAME LENGTH\n')
+if length(cVec) ~= length(dVec) % Crash if vector lengths are unequal
+    error('VECTOR LENGTHS MUST BE EQUAL: "cVec" (%d) AND "dVec" (%d)\n', ...
+        length(cVec), length(dVec))
 end
 
-meanDs = mean(dVec); % Average d' (x)
-meanCs = mean(cVec); % Average criterion (y)
+mCs = mean(cVec); % mean c (y)
+mDs = mean(dVec); % mean d' (x)
 
-sumDsqrs = sum((dVec - meanDs) .^ 2); % Sum of d' squares (x)
-sumCsqrs = sum((cVec - meanDs) .^ 2); % Sum of c squares (y)
-sumDCs = sum((dVec - meanDs) .* (cVec - meanCs)); % Sum of criterion x d' squares (y)
-    
-slope = sumDCs / sumDsqrs;
-intercept = meanCs - (slope * meanDs);
+cSq = sum((cVec - mCs) .^ 2); % sum of squares: c (y) ** FIXED: 02/16/2020 **
+dSq = sum((dVec - mDs) .^ 2); % sum of squares: d' (x)
+cdSq = sum((dVec - mDs) .* (cVec - mCs)); % sum of squares: c x d' (y)
 
-r = sumDCs / sqrt((sumDsqrs * sumCsqrs)); % pearson's r
-r2 = r ^ 2; % Variance explained (%)
+slope = cdSq / dSq; % regression slope
+int = mCs - (slope * mDs); % regression intercept
+r = cdSq / sqrt((dSq * cSq)); % Pearson's r
+r2 = r ^ 2; % explained variance (%)
+ln = slope .* dVec + int; % y = mx + b
+resid = (cVec - ln); % residuals
+nc = resid + mCs; % normalized c (residuals + mean)
 
-lineCvec = slope .* dVec + intercept; % mx + b
-
-residuals = (cVec - lineCvec); % Residuals from regression
-normCs = residuals + meanCs; % Residuals + mean
 end
-
